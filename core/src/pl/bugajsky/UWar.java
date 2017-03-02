6a13e73019d5fece7b9c93f8d8ddd4c5e3b14489
sirmarbug <maly@8086.pl>
2017-03-02 16:41:57 +0100

    Obsługa kamery - obsługiwane krańce mapy.

--- core/src/pl/bugajsky/UWar.java
+++ core/src/pl/bugajsky/UWar.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class UWar extends ApplicationAdapter {
@@ -12,6 +13,9 @@ public class UWar extends ApplicationAdapter {
 	private OrthographicCamera camera;
 	private Texture texture;
 	private Pixmap pixmap;
+
+//	Zmienna wyświetlająca obecną pozycję
+ 	private BitmapFont font;
 
 	@Override
 	public void create () {
@@ -29,6 +33,10 @@ public class UWar extends ApplicationAdapter {
 		pixmap.drawRectangle(0,0, 5000, 5000);
 		texture = new Texture(pixmap);
 		pixmap.dispose();
+
+//		napis
+		font = new BitmapFont();
+		font.setColor(Color.WHITE);
 	}
 
 	@Override
@@ -45,6 +53,10 @@ public class UWar extends ApplicationAdapter {
 
 //		rysowanie postaci gracza
 		batch.draw(player.getTexture(),player.x, player.y);
+
+//		wypisanie współrzędnych gracza
+		font.draw(batch, "x: " + player.x + "y: " + player.y, camera.position.x - 490, camera.position.y + 490);
+
 		batch.end();
 	}
 
@@ -52,26 +64,65 @@ public class UWar extends ApplicationAdapter {
 
 		camera.update();
 		batch.setProjectionMatrix(camera.combined);
-		camera.position.set(player.x, player.y, 0);
+
+//		ustawienie kamery tak aby mapa była maksymalnie do krańców ekranu
+//		ustawienie kamery z lewej strony i prawej strony
+//		operacje dla środka ekranu
+		if(player.x > 500 && player.x < 4500) {
+			if(player.y > 500 && player.y < 4500) {
+				camera.position.set(player.x, player.y, 0);
+			}else{
+				if(player.y < 500){
+					camera.position.set(player.x, 500, 0);
+				}else{
+					camera.position.set(player.x, 4500, 0);
+				}
+			}
+		}
+
+//		operacje dla krańców ekranu
+		if(player.x < 500 || player.x > 4500) {
+			if(player.y < 500 || player.y > 4500){
+				if(player.x < 500 && player.y < 500){
+					camera.position.set(500, 500, 0);
+//				}else if(player.x < 500 && player.y > 4500){
+//					camera.position.set(500, 5000, 0);
+				}
+			}else {
+				if (player.x < 500) {
+					camera.position.set(500, player.y, 0);
+				} else {
+					camera.position.set(4500, player.y, 0);
+				}
+			}
+		}
 
 		if(Gdx.input.isKeyPressed(Input.Keys.ESCAPE)){
 			Gdx.app.exit();
 		}
 
 		if(Gdx.input.isKeyPressed(Input.Keys.W) && player.y < 5000-2*player.radius){
-			player.y += 15;
+			player.y += 5;
+//			System.out.println(player.y);
 		}
 
 		if(Gdx.input.isKeyPressed(Input.Keys.S) && player.y > 0){
-			player.y -= 15;
+			player.y -= 5;
+//			System.out.println(player.y);
 		}
 
 		if(Gdx.input.isKeyPressed(Input.Keys.A) && player.x > 0){
-			player.x -= 15;
+			player.x -= 5;
+//			System.out.println(player.x);
 		}
 
 		if(Gdx.input.isKeyPressed(Input.Keys.D) && player.x < 5000-2*player.radius) {
-			player.x += 15;
+			player.x += 5;
+//			System.out.println(player.x);
+		}
+
+		if(Gdx.input.isKeyPressed(Input.Keys.D) && Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT)){
+			player.x += 20;
 		}
 
 
@@ -87,5 +138,6 @@ public class UWar extends ApplicationAdapter {
 	public void dispose () {
 		batch.dispose();
 		player.getTexture().dispose();
+		font.dispose();
 	}
 }
